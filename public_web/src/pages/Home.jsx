10c9353e2caf5569223235837168de3bdaf320fa
Kamal Meena <officalkamalmeena2005@gmail.com>
2025-11-17 13:21:42 +0530

    home done !!

--- public_web/src/pages/Home.jsx
+++ public_web/src/pages/Home.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import api from "../api/api";
+import Lottie from "lottie-react";
+import Robot3D from '../assets/RobotBot3D.json'
+import aiDigital from '../assets/aiDigital.json'
 
 export default function Home() {
   const [data, setData] = useState(null);
@@ -40,27 +43,31 @@ export default function Home() {
       </div>
 
       {/* IMAGES SECTION */}
-      <div className="flex justify-center items-center gap-12 px-20 mt-10 max-w-[1300px] mx-auto">
+      <div className="flex justify-center items-center gap-12 px-20 mt-[-40px] max-w-[1300px] mx-auto">
 
-        <div className="w-[45%]">
-          <img
-            src={data?.heroImage}
-            className="w-full  shadow-[0_3px_10px_rgba(0,0,0,0.15)]"
-            alt="main"
+        <div className="w-[60%] flex justify-center">
+          {/* aiDigital BIG (100%) */}
+          <Lottie
+            animationData={aiDigital}
+            loop={true}
+            style={{ width: "100%", height: "100%" }}
           />
         </div>
 
-        <div className="w-[39%]">
-          <img
-            src={data?.secondImage}
-            className="w-full shadow-[0_3px_10px_rgba(0,0,0,0.15)]"
-            alt="secondary"
+        <div className="w-[39%] flex justify-center">
+          {/* Robot SMALL (50%) */}
+          <Lottie
+            animationData={Robot3D}
+            loop={true}
+            style={{ width: "100%", height: "50%" }}
           />
         </div>
+
+
       </div>
 
       {/* CONTINUE BUTTON */}
-      <div className="w-full flex justify-end  mt-12 max-w-[1300px] mx-auto">
+      <div className="w-full flex justify-end mt-[-40px] max-w-[1300px] mx-auto">
         <button
           onClick={() => navigate("/about")}
           className="bg-[#009e66]  transition text-white text-xl font-regular px-14 py-3  flex items-center gap-4"
